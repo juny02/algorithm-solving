c613a305f40ba529494ab9bb22934dadfb7a5c57
juny02 <qapl12321@gmail.com>
2025-01-19 18:46:39 +0900

    Refactor(baekjoon): 2667 - simplify if logic

--- graph/2667.py
+++ graph/2667.py
@@ -2,48 +2,41 @@ import sys
 from collections import deque
 
 N = int(input())
-matrix = []
-visited = [[False]*N for _ in range(N)]
-
-for _ in range(N):
-    row = sys.stdin.readline().strip()
-    matrix.append(row)
+matrix = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(N)]
 
 
+# 방문 처리를 굳이 visited만들지 않고, matrix에서 한번에 0 으로 넣어준다
+# 백터로 처리할 수 있도록 한다!
 
-def bfs(matrix, visited, r,c) -> int:
-    queue = deque([(r,c)])
-    visited[r][c] = True
+
+def bfs(matrix, x, y):
+    queue = deque([(x, y)])
+    matrix[x][y] = 0
     length = 0
 
+    directions = [(-1, 0), (+1, 0), (0, -1), (0, +1)]
+
     while queue:
-        r, c = queue.popleft()
+        x, y = queue.popleft()
         length += 1
 
-        if c>0 and matrix[r][c-1]=='1' and not visited[r][c-1]:
-            queue.append((r,c-1))
-            visited[r][c-1] = True
-        if c<N-1 and matrix[r][c+1] == '1' and not visited[r][c+1]:
-            queue.append((r,c+1))
-            visited[r][c+1] = True
-        if r>0 and matrix[r-1][c] == '1'and not visited[r-1][c]:
-            queue.append((r-1,c))
-            visited[r-1][c] = True
-        if r<N-1 and matrix[r+1][c] =='1'and not visited[r+1][c]:
-            queue.append((r+1,c))
-            visited[r+1][c] = True
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx <= N - 1 and 0 <= ny <= N - 1 and matrix[nx][ny] == 1:
+                queue.append((nx, ny))
+                matrix[nx][ny] = 0
 
     return length
 
+
 lengths = []
 count = 0
-for i in range(0, N*N):
-    r, c = int(i/N), i%N
-    if matrix[r][c] == '1' and not visited[r][c]:
-        lengths.append(bfs(matrix, visited, r,c))
+for i in range(0, N * N):
+    r, c = int(i / N), i % N
+    if matrix[r][c] == 1:
+        lengths.append(bfs(matrix, r, c))
         count += 1
 
 print(count)
 for i in sorted(lengths):
     print(i)
-
