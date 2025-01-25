999162091606ffc403e3ff6e241d58be6e4a547a
juny02 <qapl12321@gmail.com>
2025-01-26 01:04:30 +0900

    Refactor(Baekjoon): 14502 with Backtracking

--- graph/14502.py
+++ graph/14502.py
@@ -5,46 +5,56 @@ import copy
 
 N, M = map(int, input().split())
 matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-infected_coordinates = set()
-wall_candidates = set()
-for x in range(N):
-    for y in range(M):
-        if matrix[x][y] == 2:
-            infected_coordinates.add((x, y))
-        if matrix[x][y] == 0:
-            wall_candidates.add((x, y))
+answer = 0  # global 변수 활용!
 
 
-answers = []
-def bfs(matrix, x, y):
-    queue = deque([(x, y)])
+def createWall(matrix, count):
+    if count == 3:
+        bfs(matrix)
+        return
 
+    for x in range(N):
+        for y in range(M):
+            if matrix[x][y] == 0:
+                matrix[x][y] = 1
+                createWall(matrix, count + 1)
+                matrix[x][y] = 0
+
+
+def bfs(origin_matrix):
+    global answer
+    queue = deque()
+    matrix = copy.deepcopy(origin_matrix)
+    # 2인 점들 넣기
+    for x in range(N):
+        for y in range(M):
+            if matrix[x][y] == 2:
+                queue.append((x, y))
+    #  bfs
     while queue:
         x, y = queue.popleft()
         directions = [(-1, 0), (+1, 0), (0, -1), (0, +1)]
         for dx, dy in directions:
             nx = x + dx
             ny = y + dy
-            if 0<=nx<N and 0<=ny<M and matrix[nx][ny]==0:
+            if 0 <= nx < N and 0 <= ny < M and matrix[nx][ny] == 0:
                 queue.append((nx, ny))
-                matrix[nx][ny]=2
-
-def get_safe_area(infected_coordinates, matrix):
-    for x, y in infected_coordinates:
-        bfs(matrix, x, y)
-    return sum(row.count(0) for row in matrix)
+                matrix[nx][ny] = 2
+    result = sum(row.count(0) for row in matrix)
+    answer = max(result, answer)
 
 
-def print_mat(matrix):
-    for row in matrix:
-        print(row)
-    print()
+createWall(matrix, 0)
+print(answer)
 
-for wall_coordinates in combinations(wall_candidates, 3):
-    copied_matrix = copy.deepcopy(matrix)
-    for x, y in wall_coordinates:
-        copied_matrix[x][y]=1
-    # print_mat(copied_matrix)
-    answers.append(get_safe_area(infected_coordinates, copied_matrix))
-    # print_mat(copied_matrix)
-print(max(answers))
+
+"""
+bfs활용
+-> count하기
+!! 중요한점 !! 미리 queue에 infected인 애들을 넣어둔다!
+
+벽세우기 -> 완전탐색
+-> 이때 백트래킹 - 최대한 후보를 줄이도록 한다
+
+
+"""
