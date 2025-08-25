9bfce3666a5756de554bad4b154a6e7f0be04bcd
juny02 <qapl12321@gmail.com>
2025-08-25 13:07:11 +0900

    bfs(b): 7569 bfs

--- graph/7569.py
+++ graph/7569.py
@@ -1,60 +1,39 @@
 from collections import deque
 
 M, N, H = map(int, input().split())
-tomatos = [list(map(int, input().split())) for _ in range(N * H)]
+tomatos = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
+# HxNxM [z][y][x]
 
 
-def bfs(graph, points, count):
-    queue = deque([points])  # 리스트의 리스트
-    visited = set(points)
-
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    hop_directions = [(0, N), (0, -N)]
+def bfs(graph, queue: deque):
+    global unripen
+    global sol
+    directions = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
     while queue:
-        add = []
-        visited |= set(add)
-        for x, y in queue.popleft():
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if (y//N)*N <= ny < (y//N+1)*N and 0<=ny<N*H and 0 <= nx < M and (nx, ny) not in visited and graph[ny][nx] >= 0:
-                    if graph[ny][nx] == 0:
-                        graph[ny][nx] = graph[y][x] + 1
-                        add.append((nx, ny))
-                    else:
-                        if graph[y][x] + 1 < graph[ny][nx]:
-                            add.append((nx, ny))
-                    count = max(graph[ny][nx], count)
-            for dx, dy in hop_directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= ny < H*N and 0 <= nx < M and (nx, ny) not in visited and graph[ny][nx] >= 0:
-                    if graph[ny][nx] == 0:
-                        graph[ny][nx] = graph[y][x] + 1
-                        add.append((nx, ny))
-                    else:
-                        if graph[y][x] + 1 < graph[ny][nx]:
-                            add.append((nx, ny))
-                    count = max(graph[ny][nx], count)
-        if len(add) != 0:
-            queue.append(list(set(add)))
-    return count
+        x, y, z = queue.popleft()
+        for dx, dy, dz in directions:
+            nx, ny, nz = x + dx, y + dy, z + dz
+            if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H and graph[nz][ny][nx] == 0:
+                graph[nz][ny][nx] = graph[z][y][x] + 1
+                queue.append((nx, ny, nz))
+                unripen -= 1
+                sol = max(graph[nz][ny][nx], sol)
 
 
-count = 0
-points = []
-for x in range(M):
-    for y in range(N*H):
-        if tomatos[y][x] == 1:
-            points.append((x, y))
-count = bfs(tomatos, points, count)
-for tomato in tomatos:
-    if 0 in tomato:
-        print(-1)
-        exit()
-if count == 0:
+queue = deque([])
+unripen = 0
+sol = 0
+for z in range(H):
+    for y in range(N):
+        for x in range(M):
+            if tomatos[z][y][x] == 1:
+                queue.append((x, y, z))
+            elif tomatos[z][y][x] == 0:
+                unripen += 1
+bfs(tomatos, queue)
+if unripen != 0:
+    print(-1)
+elif sol == 0:
     print(0)
 else:
-    print(count - 1)
-
-    """
-    다시방문 안한데를 방문하ㅣㄱ 위해 visited를 따로 둠 graph에 갱신하는거로못하고
-    """
+    print(sol-1)
